3f6af6fa2f092cdba4fb826e7c28198f20cad1d1
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-30 12:52:57 -0300

    feat(person): injeta os serviços e o automapper

---
+++ DigitalBank.Application/Profiles/PersonProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DigitalBank.Application.DTOs.Person;
+using DigitalBank.Domain.Entities;
+
+namespace DigitalBank.Application.Profiles;
+
+public class PersonProfile : Profile
+{
+	public PersonProfile()
+	{
+		CreateMap<CreatePersonDto, Person>().ReverseMap();
+		CreateMap<ReadPersonDto, Person>().ReverseMap();
+		CreateMap<UpdatePersonDto, Person>().ReverseMap();
+	}
+}
--- DigitalBank.Infra.ioC/DependencyInjection.cs
+++ DigitalBank.Infra.ioC/DependencyInjection.cs
@@ -1,4 +1,9 @@
-﻿using DigitalBank.Infra.Data.Context;
+﻿using DigitalBank.Application.Contracts.Services;
+using DigitalBank.Application.Profiles;
+using DigitalBank.Application.Services;
+using DigitalBank.Domain.Contracts.Repositories;
+using DigitalBank.Infra.Data.Context;
+using DigitalBank.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +18,16 @@ public static class DependencyInjection
         
         services.AddEntityFrameworkSqlServer()
         .AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+        services.AddScoped<IPersonRepository, PersonRepository>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddServices (this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAutoMapper(typeof(PersonProfile));
+        services.AddScoped<IPersonService, PersonService>();
 
         return services;
     }
