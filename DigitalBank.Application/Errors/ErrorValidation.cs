23a8c395d11e3fb5cbd0e56b2c1d7f5432b6df19
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-30 10:25:38 -0300

    feat: cria serviço para lidar com os resultados dos serviços da aplicacao

---
+++ DigitalBank.Application/Errors/ErrorValidation.cs
@@ -0,0 +1,7 @@
+﻿namespace DigitalBank.Application.Errors;
+
+public class ErrorValidation
+{
+    public string Field { get; set; }
+    public string Message { get; set; }
+}
---
+++ DigitalBank.Application/Services/ResultService.cs
@@ -0,0 +1,45 @@
+﻿using DigitalBank.Application.Errors;
+using FluentValidation.Results;
+
+namespace DigitalBank.Application.Services;
+
+public class ResultService
+{
+    public bool Success { get; set; } = true;
+    public string Message { get; set; }
+    public ICollection<ErrorValidation> Errors { get; set; }
+    public int Status { get; set; }
+
+    public static ResultService RequestError(string message, int status, ValidationResult validationResult)
+    {
+        return new ResultService
+        {
+            Success = false,
+            Message = message,
+            Status = status,
+            Errors = validationResult.Errors.Select(x => new ErrorValidation { Field = x.PropertyName, Message = x.ErrorMessage }).ToList()
+        };
+    }
+
+    public static ResultService<T> RequestError<T>(string message, int status, ValidationResult validationResult)
+    {
+        return new ResultService<T>
+        {
+            Success = false,
+            Message = message,
+            Status = status,
+            Errors = validationResult.Errors.Select(x => new ErrorValidation { Field = x.PropertyName, Message = x.ErrorMessage }).ToList()
+        };
+    }
+
+    public static ResultService Fail(string message, int status) => new ResultService { Success = false, Message = message, Status = status };
+    public static ResultService<T> Fail<T>(string message, int status) => new ResultService<T> { Success = false, Message = message, Status = status };
+
+    public static ResultService Ok(string message, int status) => new ResultService { Message = message, Success = true, Status = status };
+    public static ResultService<T> Ok<T>(T data, int status) => new ResultService<T> { Data = data, Success = true, Status = status };
+}
+
+public class ResultService<T> : ResultService
+{
+    public T Data { get; set; }
+}
