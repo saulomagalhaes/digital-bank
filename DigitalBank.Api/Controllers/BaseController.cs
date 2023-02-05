a10f61b950f2a12ff3a01656f7249c2ca872d1d8
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-05 17:11:32 -0300

    feat: cria um controlador base para validar permissoes

---
+++ DigitalBank.Api/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalBank.Api.Controllers;
+
+[Authorize]
+[ApiController]
+public class BaseController : ControllerBase
+{
+    [NonAction]
+    public bool ValidPermission(List<string> permissionUser, List<string> permissionNeeded)
+    {
+        return permissionNeeded.Any(x => permissionUser.Contains(x));
+    }
+
+    [NonAction]
+    public IActionResult Forbidden()
+    {
+        var obj = new
+        {
+            code = "permission_denied",
+            message = "usuário não tem permissão para acessar esse recurso"
+        };
+
+        return new ObjectResult(obj) { StatusCode = 403 };
+    }
+}
