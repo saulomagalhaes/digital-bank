a3f5d0a7f5e6620b24526bca1c18be858177c976
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-05 15:56:28 -0300

    feat: cria a entidade persmission

---
+++ DigitalBank.Domain/Entities/Permission.cs
@@ -0,0 +1,8 @@
+﻿namespace DigitalBank.Domain.Entities;
+
+public class Permission
+{
+    public int Id { get; set; }
+    public string VisualName { get; set; }
+    public string PermissionName { get; set; }
+}
