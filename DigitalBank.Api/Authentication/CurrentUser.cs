c7d6d008c7b4d03a4ff508a65851cc9599f42eb4
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-05 17:11:20 -0300

    feat: cria a classe currentuser

---
+++ DigitalBank.Api/Authentication/CurrentUser.cs
@@ -0,0 +1,33 @@
+﻿using DigitalBank.Domain.Contracts.Authentication;
+
+namespace DigitalBank.Api.Authentication;
+
+public class CurrentUser : ICurrentUser
+{
+    public CurrentUser(IHttpContextAccessor httpContextAccessor)
+    {
+        var httpContext = httpContextAccessor.HttpContext;
+        var claims = httpContext.User.Claims;
+
+        if(claims.Any(x => x.Type == "id"))
+        {
+            var id = Convert.ToInt32(claims.First(x => x.Type == "id").Value);
+            Id = id;
+        }
+
+        if (claims.Any(x => x.Type == "email"))
+        {
+            var email = claims.First(x => x.Type == "email").Value;
+            Email = email;
+        }
+
+        if (claims.Any(x => x.Type == "permissions"))
+        {
+            var permissions = claims.First(x => x.Type == "permissions").Value;
+            Permissions = permissions;
+        }
+    }
+    public int Id { get ; set; }
+    public string Email { get; set; }
+    public string Permissions { get ; set ; }
+}
