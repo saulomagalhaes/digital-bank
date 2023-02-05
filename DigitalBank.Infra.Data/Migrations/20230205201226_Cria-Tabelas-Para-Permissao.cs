b32d0ade565499df4ed2ad0be557d69ab84ca286
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-05 17:26:38 -0300

    feat: cria as migrations para permissions e userpermissions

---
+++ DigitalBank.Infra.Data/Migrations/20230205201226_Cria-Tabelas-Para-Permissao.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigitalBank.Infra.Data.Migrations
+{
+    public partial class CriaTabelasParaPermissao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "permissions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    visual_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    permission_name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_permissions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "userPermission",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    permission_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userPermission", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_userPermission_permissions_permission_id",
+                        column: x => x.permission_id,
+                        principalTable: "permissions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_userPermission_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userPermission_permission_id",
+                table: "userPermission",
+                column: "permission_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userPermission_user_id",
+                table: "userPermission",
+                column: "user_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "userPermission");
+
+            migrationBuilder.DropTable(
+                name: "permissions");
+        }
+    }
+}
