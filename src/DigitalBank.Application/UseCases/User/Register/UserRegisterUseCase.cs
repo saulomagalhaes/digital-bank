859e1ce261b0ebe3ec4f0ed526978654e55a3f75
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-03-26 17:25:35 -0300

    feat(registrar usuario): adiciona usecase e o validator

---
+++ src/DigitalBank.Application/UseCases/User/Register/UserRegisterUseCase.cs
@@ -0,0 +1,22 @@
+﻿using DigitalBank.Communication.Requests;
+
+namespace DigitalBank.Application.UseCases.User.Register;
+
+public class UserRegisterUseCase
+{
+    public Task Execute(RequestRegisterUserJson request)
+    {
+
+    }
+
+    private void Validate(RequestRegisterUserJson request)
+    {
+        var validator = new UserRegisterValidator().Validate(request);
+
+        if (!validator.IsValid)
+        {
+            var errorMessages = validator.Errors.Select(error => error.ErrorMessage).ToList();
+            throw new Exception();
+        }
+    }
+}
---
+++ src/DigitalBank.Application/UseCases/User/Register/UserRegisterValidator.cs
@@ -0,0 +1,23 @@
+﻿using DigitalBank.Communication.Requests;
+using DigitalBank.Exceptions;
+using FluentValidation;
+
+namespace DigitalBank.Application.UseCases.User.Register;
+
+public class UserRegisterValidator : AbstractValidator<RequestRegisterUserJson>
+{
+    public UserRegisterValidator()
+    {
+        RuleFor(u => u.Name).NotEmpty().WithMessage(ResourceErrorMessages.NOME_USUARIO_VAZIO);
+        
+        RuleFor(u => u.Email).NotEmpty().WithMessage(ResourceErrorMessages.EMAIL_USUARIO_VAZIO);
+        When(u => !string.IsNullOrWhiteSpace(u.Email), () =>
+        {
+            RuleFor(u => u.Email).EmailAddress().WithMessage(ResourceErrorMessages.EMAIL_USUARIO_INVALIDO);
+        });
+
+        RuleFor(u => u.Password).NotEmpty().WithMessage(ResourceErrorMessages.SENHA_USUARIO_VAZIO);
+        RuleFor(u => u.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceErrorMessages.SENHA_USUARIO_MINIMO_SEIS_CARACTERES);
+
+    }
+}
