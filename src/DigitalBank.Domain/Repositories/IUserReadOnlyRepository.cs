109a36ae4140c8009aff85a21eba1c03cfae8e95
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-03-28 11:07:56 -0300

    feat(login): adiciona método ai repositório para login

--- src/DigitalBank.Domain/Repositories/IUserReadOnlyRepository.cs
+++ src/DigitalBank.Domain/Repositories/IUserReadOnlyRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace DigitalBank.Domain.Repositories;
+﻿using DigitalBank.Domain.Entities;
+
+namespace DigitalBank.Domain.Repositories;
 
 public interface IUserReadOnlyRepository
 {
     Task<bool> UserExistsWithEmail(string email);
+    Task<User> Login(string email, string password);
 }
--- src/DigitalBank.Infra/Repositories/UserRepository.cs
+++ src/DigitalBank.Infra/Repositories/UserRepository.cs
@@ -19,6 +19,13 @@ public class UserRepository : IUserReadOnlyRepository, IUserWriteOnlyRepository
         await _context.Users.AddAsync(user);
     }
 
+    public async Task<User> Login(string email, string password)
+    {
+        return await _context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Email == email && x.Password == password);
+    }
+
     public async Task<bool> UserExistsWithEmail(string email)
     {
         return await _context.Users.AnyAsync(x => x.Email == email);
